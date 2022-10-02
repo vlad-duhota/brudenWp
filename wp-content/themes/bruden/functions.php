150e07a2820b77e2ccfc7f505b15ade819949b49
ivan-medvedew <mediol26@gmail.com>
2022-10-02 18:58:51 +0300

    deleted plugins && carbon off

--- wp-content/themes/bruden/functions.php
+++ wp-content/themes/bruden/functions.php
@@ -52,18 +52,18 @@ function site_scripts() {
     // }
 }
 
-// Carbon Fields
-add_action( 'after_setup_theme', 'crb_load' );
-function crb_load() {
-	require_once( 'includes/carbon-fields/vendor/autoload.php' );
-	\Carbon_Fields\Carbon_Fields::boot();
-}
+//// Carbon Fields
+// add_action( 'after_setup_theme', 'crb_load' );
+// function crb_load() {
+// 	require_once( 'includes/carbon-fields/vendor/autoload.php' );
+// 	\Carbon_Fields\Carbon_Fields::boot();
+// }
 
-add_action('carbon_fields_register_fields', 'register_carbon_fields');
-function register_carbon_fields () {
-    require_once('includes/carbon-fields-options/theme-options.php');
-    require_once('includes/carbon-fields-options/post-meta.php');
-}
+// add_action('carbon_fields_register_fields', 'register_carbon_fields');
+// function register_carbon_fields () {
+//     require_once('includes/carbon-fields-options/theme-options.php');
+//     require_once('includes/carbon-fields-options/post-meta.php');
+// }
 
 // Theme support
 add_theme_support( 'title-tag' );
