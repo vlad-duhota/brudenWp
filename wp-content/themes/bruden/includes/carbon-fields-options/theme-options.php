a00e4ad089cb110ae1969e2e3f379703427b8e74
ivan-medvedew <mediol26@gmail.com>
2022-11-01 16:58:06 +0200

    sale && suscribe title fixed

--- wp-content/themes/bruden/includes/carbon-fields-options/theme-options.php
+++ wp-content/themes/bruden/includes/carbon-fields-options/theme-options.php
@@ -37,7 +37,7 @@ Container::make('theme_options', 'Theme options')
     Field::make( 'text', 'title_1', 'Categories title' ),
     Field::make( 'text', 'title_2', 'Deal Of The Week title' ),
     Field::make( 'text', 'title_3', 'Latest News title' ),
-    Field::make( 'text', 'title_4', 'Special Products title' ),
+    // Field::make( 'text', 'title_4', 'Special Products title' ),
     Field::make( 'separator', 'crb_separator_banner_1', 'First banner'  ),
     Field::make( 'text', 'banner_1_title', 'Banner title' )
     ->set_width(80),
